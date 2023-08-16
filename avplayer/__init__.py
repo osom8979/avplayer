597864f3955d060b9845de23eae624d1d6256e4f
zer0 <osom8979@gmail.com>
2023-08-16 20:11:39 +0900

    Update version: 1.3.0

--- avplayer/__init__.py
+++ avplayer/__init__.py
@@ -5,7 +5,7 @@ from avplayer.apps.default import default_main as av_main
 from avplayer.av.av_io import AvIo
 from avplayer.config import Config as AvConfig
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __all__ = [
     "AioApp",
     "AioWebApp",
