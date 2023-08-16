09f234c9d615c6863369e94c1af12d73fc528274
zer0 <osom8979@gmail.com>
2023-08-16 20:11:01 +0900

    Add attribute: __all__

--- avplayer/__init__.py
+++ avplayer/__init__.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from avplayer.apps.default import AioApp, AioWebApp, IoApp
+from avplayer.apps.default import default_main as av_main
+from avplayer.av.av_io import AvIo
+from avplayer.config import Config as AvConfig
+
 __version__ = "1.2.1"
+__all__ = [
+    "AioApp",
+    "AioWebApp",
+    "AvConfig",
+    "AvIo",
+    "IoApp",
+    "av_main",
+]
--- avplayer/apps/default.py
+++ avplayer/apps/default.py
@@ -2,6 +2,7 @@
 
 from argparse import Namespace
 from asyncio.exceptions import CancelledError
+from inspect import iscoroutinefunction
 from typing import Optional
 
 from numpy import uint8
@@ -22,49 +23,76 @@ from avplayer.logging.logging import logger
 
 
 class IoApp(AvAppBase, AvInterface):
-    def __init__(self, config: Config):
+    def __init__(self, config: Config, coro=None):
         super().__init__(config, self)
+        self._coro = coro
 
     @override
     def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
-        return image
+        if self._coro is not None:
+            return self._coro(image)
+        else:
+            return image
 
 
 class AioApp(AsyncAvAppBase, AsyncAvInterface):
-    def __init__(self, config: Config):
+    def __init__(self, config: Config, coro=None):
         super().__init__(config, self)
+        self._is_coroutine = iscoroutinefunction(coro)
+        self._coro = coro
 
     @override
     async def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
-        return image
+        if self._coro is not None:
+            if self._is_coroutine:
+                return await self._coro(image)
+            else:
+                return self._coro(image)
+        else:
+            return image
 
 
 class AioWebApp(AsyncAvWebAppBase, AsyncAvWebInterface):
-    def __init__(self, config: Config):
+    def __init__(self, config: Config, coro=None, keydown=None):
         super().__init__(config, self)
+        self._is_coroutine = iscoroutinefunction(coro)
+        self._coro = coro
+        self._is_coroutine_keydown = iscoroutinefunction(keydown)
+        self._keydown = keydown
 
     @override
     async def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
-        return image
+        if self._coro is not None:
+            if self._is_coroutine:
+                return await self._coro(image)
+            else:
+                return self._coro(image)
+        else:
+            return image
 
     @override
     async def on_key_pressed(
         self, keycode: str, shift: bool, ctrl: bool, alt: bool
     ) -> None:
-        pass
+        if self._keydown is None:
+            return
+        if self._is_coroutine_keydown:
+            await self._keydown(keycode, shift, ctrl, alt)
+        else:
+            self._keydown(keycode, shift, ctrl, alt)
 
 
-def default_main(args: Namespace) -> int:
+def default_main(args: Namespace, coro=None, keydown=None) -> int:
     config = Config.from_namespace(args)
     config.logging_params()
 
     try:
         if config.is_io:
-            IoApp(config).start_app()
+            IoApp(config, coro).start_app()
         elif config.is_aio:
-            aio_run(AioApp(config).start_async_app())
+            aio_run(AioApp(config, coro).start_async_app())
         elif config.is_aioweb:
-            AioWebApp(config).start_webserver_with_avio()
+            AioWebApp(config, coro, keydown).start_webserver_with_avio()
         else:
             raise NotImplementedError
     except CancelledError:
