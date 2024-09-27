5c20f75a17fbae649832508b49c97b33282f21d1
zer0 <osom8979@gmail.com>
2024-09-27 14:16:47 +0900

    Bump version

--- avplayer/__init__.py
+++ avplayer/__init__.py
@@ -5,7 +5,7 @@ from avplayer.apps.defaults import AioApp, AioCv, AioTk, IoApp
 from avplayer.av.av_io import AvIo
 from avplayer.avconfig import AvConfig
 
-__version__ = "1.8.2"
+__version__ = "1.9.0"
 __all__ = [
     "__version__",
     "AioApp",
